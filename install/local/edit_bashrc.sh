e235414f1f05079b3cca370cc1945d9b14df4381
Feng Hao <howiefh@gmail.com>
2014-03-11 18:05:47 +0800

    modified:   install/local/edit_bashrc.sh show git branch in bash

--- install/local/edit_bashrc.sh
+++ install/local/edit_bashrc.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-sed -i "s/PS1=.*/PS1='\\\[\\\e\[1;32m\\\]\[\\\u@\\\A \\\w]\\\\$\\\[\\\e\[0m\\\] \'/" $HOME/.bashrc
-sudo sed -i "s/PS1=.*/PS1='\\\[\\\e\[1;31m\\\]\[\\\u@\\\A \\\w]\\\\$\\\[\\\e\[0m\\\] \'/" /root/.bashrc
+# sed -i "s/PS1=.*/PS1='\\\[\\\e\[1;32m\\\]\[\\\u@\\\A \\\w]\\\\$\\\[\\\e\[0m\\\] \'/" $HOME/.bashrc
+# sudo sed -i "s/PS1=.*/PS1='\\\[\\\e\[1;31m\\\]\[\\\u@\\\A \\\w]\\\\$\\\[\\\e\[0m\\\] \'/" /root/.bashrc
+
+echo "parse_git_branch() {" >> file
+echo	"git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'" >> file
+echo "}" >> file
+echo "PS1='\[\e[1;32m\][\u@\A \w\$(parse_git_branch)]\\$\[\e[0m\] '"  >> file
